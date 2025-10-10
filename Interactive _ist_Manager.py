f18014d8f9125ab4022e490d3afb6570e75378bc
gurgenaim2124 <Gurgena>
2025-10-10 04:27:27 +0400

    interactive_ist_manager done

--- Interactive _ist_Manager.py
+++ Interactive _ist_Manager.py
@@ -1,11 +1,11 @@
 user_list = []
 
 while True:
-    user_input = (input("Add a number to the list or 'Quit': "))
+    user_input = (input("Add a number to the list: "))
 
-    if user_input.lower() == "quit":
-        print("Final list", user_list)
-        break
+    # if user_input.lower() == "quit":
+    #     print("Final list", user_list)
+    #     break
 
     numbers = user_input.split(',')
 
@@ -22,10 +22,9 @@ while True:
     user_remove_number = input("do you want to remove a number from the list?: ")
 
     if user_remove_number.lower() == "yes":
-        number_to_remove = input("Enter which number do you want to delete? ")
+        number_to_remove = input("Enter which number do you want to delete? ").strip()
    
         if number_to_remove.isdigit():
-            sum_list = 0
             number_to_remove = int(number_to_remove)
             if number_to_remove in user_list:
                 user_list.remove(number_to_remove)
@@ -36,14 +35,45 @@ while True:
         else :
             print("invalid input try again")
     elif user_remove_number.lower() == "no":
-        print("okay goodbye")
-        break
+        print("okay continuing...")
+
+        
     
+
+    # clear or not
+
+    list_cleaner = input("do you want to clear the list ? ")
+
+    if list_cleaner.lower() == "yes":
+        user_list.clear()
+        print("List has been cleared completely!")
+    elif list_cleaner.lower() == "no":
+        print("Okay continuing...")
+    elif list_cleaner == int or float:
+        print("Please enter yes or no")
+    else:
+        print("Please enter valid sentence")
+
     # sum and average
 
     if user_list:
         total = sum(user_list)
         average = round(total / len(user_list))
         print(f"Sum: {total} and Average: {average}")
+        print(f"Minimum value: {min(user_list)} and Maximum value: {max(user_list)}")
     else:
         print("No numbers in the list")
+        
+    # quiting 
+
+    user_exit = input("Do you want to exit the program ? ")
+
+    if user_exit.lower() == "yes":
+        print("You've successfully exited the program") 
+        print("Goodbye...")
+        break
+    elif user_exit.lower() == "no":
+        print("Okay start again...")
+        continue
+    else:
+        print("Please enter valid sentence")
